024b8c93e771b624315bd2930bbdc7d65c1ef833
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2013-07-26 12:10:39 +0200

    Rails 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Panorama::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # small change
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
