5d8acd09981ca2c02b308651f9050d613056f821
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2013-09-09 18:33:38 +0200

    Merge commit '768c7895f0c1b341c24065d76ebda8f1b9ec559f'
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	Panorama.war
    	app/helpers/env_helper.rb
    	app/views/active_session_history/_list_session_statistics_historic_grouping_table_def.html.erb
    	config/environments/production.rb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -226,7 +226,7 @@ function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_a
          items: [
             {
                 label: (show_y_axes==true ? "y-Achse(n) ausblenden" : "y-Achse(n) anzeigen"),
-                icon: 'images/application-monitor.png',
+                icon: 'assets/images/application-monitor.png',
                 action: function(){
                     jQuery('#'+plot_area_id).html(""); // Altes Diagramm entfernen
                     plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_axes, (show_y_axes==true ? false : true), x_axis_time);
@@ -234,7 +234,7 @@ function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_a
             },
              {
                  label: (multiple_y_axes==true ? "Alle Kurven in einer y-Achse darstellen" : "Eigene y-Achse je Kurve (100% Wertebereich)"),
-                 icon: 'images/application-monitor.png',
+                 icon: 'assets/images/application-monitor.png',
                  action: function(){
                      jQuery('#'+plot_area_id).html(""); // Altes Diagramm entfernen
                      plot_diagram(unique_id, plot_area_id, caption, data_array, (multiple_y_axes==true ? false : true), show_y_axes, x_axis_time);
