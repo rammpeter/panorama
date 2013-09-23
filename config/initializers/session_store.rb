5e8e8ac288ac9b6532503899fd9ce428a37cf576
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2013-09-23 15:35:41 +0200

    Commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,3 +7,5 @@ Panorama::Application.config.session_store :cookie_store, key: '_Panorama_sessio
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Panorama::Application.config.session_store :active_record_store
+
+
