d4da16e2b00b4751ad5aed0ce6c696b5dbcbd6a2
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2013-10-21 18:14:55 +0200

    Verlinken Table structure

--- start_Panorama_jetty.sh
+++ start_Panorama_jetty.sh
@@ -2,5 +2,5 @@ export PANORAMA_HOME=$PWD
 export PANORAMA_USAGE_LOG=$PANORAMA_HOME/Usage.log
 export LOG=$PANORAMA_HOME/Panorama_jetty.log
 echo "Starting Panorama with jetty, logfile is $LOG"
-java -XX:MaxPermSize=256M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -jar $PANORAMA_HOME/Panorama.war 2>&1 | tee -a $LOG
+java -Xmx1024m -XX:MaxPermSize=512M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -Djetty.port=8090 -jar $PANORAMA_HOME/Panorama.war 2>&1 | tee -a $LOG
 
