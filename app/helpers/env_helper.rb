1d256ef8124b07b3ffe9aef97d58324d37b5e902
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2013-11-13 13:46:40 +0100

    Cookies verschlüsseln

--- app/helpers/env_helper.rb
+++ app/helpers/env_helper.rb
@@ -1,17 +1,17 @@
 # encoding: utf-8
 module EnvHelper
-  #require "Encryptor"
 
   # Einlesen last_logins aus cookie-store
   def read_last_login_cookies
     begin
       if cookies[:last_logins]
-        cookies_last_logins = Marshal.load(cookies[:last_logins])
+        crypt = ActiveSupport::MessageEncryptor.new(Panorama::Application.config.secret_key_base)
+        cookies_last_logins = crypt.decrypt_and_verify(cookies[:last_logins])
       else
         cookies_last_logins = []
       end
-    rescue Exception
-      cookies_last_logins = []      # Cookie neu initialisieren wenn Fehler beim Auslesen
+#    rescue Exception
+#      cookies_last_logins = []      # Cookie neu initialisieren wenn Fehler beim Auslesen
     end
     cookies_last_logins = [] unless cookies_last_logins.instance_of?(Array)  # Falscher Typ des Cookies?
 
@@ -30,7 +30,9 @@ module EnvHelper
 
   # Zurückschreiben des Cookies in cookie-store
   def write_last_login_cookies(cookies_last_logins)
-    cookies.permanent[:last_logins] =  Marshal.dump(cookies_last_logins)           # Zurückschreiben des Cookies
+
+    crypt = ActiveSupport::MessageEncryptor.new(Panorama::Application.config.secret_key_base)
+    cookies[:last_logins] = crypt.encrypt_and_sign(cookies_last_logins)
   end
 
 
