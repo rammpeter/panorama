b0d6cf8e991d3ceb48a07833081a2defcf10d536
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2014-03-13 11:57:05 +0100

    update_area aus Methode

--- start_Panorama_jetty.sh
+++ start_Panorama_jetty.sh
@@ -3,5 +3,8 @@ export PANORAMA_PORT=8090
 export PANORAMA_USAGE_LOG=$PANORAMA_HOME/Usage.log
 export LOG=$PANORAMA_HOME/Panorama_jetty.log
 echo "Starting Panorama with jetty, logfile is $LOG"
-java -Xmx1024m -XX:MaxPermSize=512M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -Djetty.port=8090 -Djetty.requestHeaderSize=8192 -Djava.io.tmpdir=./work -jar $PANORAMA_HOME/Panorama.war 2>&1 | tee -a $LOG 
+
+# -Xcompile.invokedynamic=true sichert für Java7, dass folgender Fehler eliminiert wird: java.lang.ClassNotFoundException: org.jruby.ext.krypt.asn1.RubyAsn1
+
+java -Xcompile.invokedynamic=true -Xmx1024m -XX:MaxPermSize=512M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -Djetty.port=8090 -Djetty.requestHeaderSize=8192 -Djava.io.tmpdir=./work -jar $PANORAMA_HOME/Panorama.war 2>&1 | tee -a $LOG 
 
