33bd0fcb499bb0ffd6e0b88ef878e80bf591fc23
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2014-12-04 10:30:28 +0100

    Version

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
   VERSION = "2.0.16"
-  RELEASE_DATE = Date.parse('2014-11-25')
+  RELEASE_DATE = Date.parse('2014-12-04')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
