da5cba42c86416cba76fe4a427289e9ef4425f69
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-01-06 13:28:57 +0100

    Initiale commit

---
+++ build_war.sh
@@ -0,0 +1,17 @@
+if [ "$1" != "without_clean_cache" ]
+then
+  echo "Clean Cache"
+  rm -r tmp/cache/assets/*
+fi
+
+if [ "$1" != "without_assets" ]
+then
+  echo "Compile assets"
+  jruby -S rake assets:precompile
+fi
+
+echo "Create Panorama.war"
+jruby -S warble 
+
+echo "Entfernen der assets unter public"
+rm -r public/assets/*
---
+++ start_Panorama_jetty.sh
@@ -0,0 +1,11 @@
+export PANORAMA_HOME=$PWD
+export PANORAMA_PORT=8090
+export PANORAMA_USAGE_LOG=$PANORAMA_HOME/Usage.log
+export LOG=$PANORAMA_HOME/Panorama_jetty.log
+echo "Starting Panorama with jetty, logfile is $LOG"
+
+# -Xcompile.invokedynamic=true sichert für Java7, dass folgender Fehler eliminiert wird: java.lang.ClassNotFoundException: org.jruby.ext.krypt.asn1.RubyAsn1
+# -Djruby.compile.invokedynamic=true statt -Xcompile.invokedynamic=true da dies für IBM-JRE 7 nicht erlaubt ist
+
+java -Xmx1024m -XX:MaxPermSize=512M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -Djruby.compile.invokedynamic=true -Djetty.port=8090 -Djetty.requestHeaderSize=8192 -Djava.io.tmpdir=./work -jar $PANORAMA_HOME/Panorama.war 2>&1 | tee -a $LOG 
+
