324ec1ac8d81e4a81cd78b2a35e33c5897bf62ae
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-01-06 10:41:24 +0100

    Debug Session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-#Panorama::Application.config.session_store :cookie_store, key: '_Panorama_session'
+Rails.application.config.session_store :cookie_store, key: '_Panorama_session'
 
 # Session in memory speichern mit Lifetime = html-Session
-Panorama::Application.config.session_store :cache_store
+# Funktioniert ab Rails 4.2.0 nicht mehr mit Jetty, daher zurück auf cookie_store
+# Panorama::Application.config.session_store :cache_store
 
 
 # Use the database for sessions instead of the cookie-based default,
