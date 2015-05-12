dcd98998fbbaecfdcaa8c1c10644291ebfaa5f12
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-05-12 09:17:05 +0200

    Prepare dragnet for html generation, URL with Suffix supported

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.0.59'
-  RELEASE_DATE = Date.parse('2015-05-11 ')
+  VERSION = '2.0.60'
+  RELEASE_DATE = Date.parse('2015-05-12 ')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
