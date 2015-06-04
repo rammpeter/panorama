bf71d00abb28dbc50c276fefba8102104738cbfc
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-06-04 09:14:30 +0200

    Javascript Fix for internet explorer

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.0.68'
-  RELEASE_DATE = Date.parse('2015-06-01')
+  VERSION = '2.0.69'
+  RELEASE_DATE = Date.parse('2015-06-04')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
