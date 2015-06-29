807f8ac5b78289517ccc920c393dac48c323b038
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-06-29 08:45:46 +0200

    Filter on username for active session history

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.0.72'
-  RELEASE_DATE = Date.parse('2015-06-23')
+  VERSION = '2.0.73'
+  RELEASE_DATE = Date.parse('2015-06-29')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
