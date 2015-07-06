838e2a91331da6a7a8e4b6ca6ee7fd58ae2a4f8f
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-07-06 17:01:25 +0200

    Version

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.0.74'
-  RELEASE_DATE = Date.parse('2015-07-01')
+  VERSION = '2.0.75'
+  RELEASE_DATE = Date.parse('2015-07-06')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
