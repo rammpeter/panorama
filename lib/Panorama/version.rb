8b6edd9c91642166e54ab9d6ba03d95bf5ef5f7c
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-09-15 17:11:22 +0200

    FORCE SERIAL in Dragnet

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.0.87'
-  RELEASE_DATE = Date.parse('2015-09-10')
+  VERSION = '2.0.88'
+  RELEASE_DATE = Date.parse('2015-09-15')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
