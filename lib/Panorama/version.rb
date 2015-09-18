804ef2c90aec17709ed551e8783dedce48cffde4
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-09-18 12:40:52 +0200

    Show last analyzed in execution plan

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.0.88'
-  RELEASE_DATE = Date.parse('2015-09-15')
+  VERSION = '2.0.89'
+  RELEASE_DATE = Date.parse('2015-09-18')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
