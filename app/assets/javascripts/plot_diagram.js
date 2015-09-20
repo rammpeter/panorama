d1a42867c9ef9b24f04b2e73feea9274e7a2bb60
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-09-20 21:40:18 +0200

    Show diagram legend also after resize

--- app/assets/javascripts/plot_diagram.js
+++ app/assets/javascripts/plot_diagram.js
@@ -10,8 +10,119 @@
 // multiple_y_axes  bool
 // show_y_axes      bool
 // x_axis_time      bool    Ist X-Achse ein Zeitstempel oder Nummer
-function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_axes, show_y_axes, x_axis_time, locale) {
-    var plot_area = jQuery('#'+plot_area_id);
+
+function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_axes, show_y_axes, x_axis_time, locale){
+    var p = new plot_diagram_class(unique_id, plot_area_id, caption, data_array, multiple_y_axes, show_y_axes, x_axis_time, locale);
+    p.initialize();
+    return p;
+}
+
+function plot_diagram_class(unique_id, plot_area_id, caption, data_array, multiple_y_axes, show_y_axes, x_axis_time, locale) {
+    var plot_area               = jQuery('#'+plot_area_id);
+    var canvas_id               = "canvas_" + unique_id;
+    var head_id                 = "head_" + canvas_id;
+    var canvas_height           = 450;
+    var updateLegendTimeout     = null;
+    var latestPosition          = null;
+    var legends                 = null          // erzeugt in initialize
+    var legendXAxis             = null;         // erzeugt in initialize
+    var previousToolTipPoint    = null;
+    var toolTipID               = canvas_id+"_ToolTip"
+    var plot                    = null;           //jQuery.plot(jQuery('#'+canvas_id), erzeugt in intialize()
+
+    // Initialisierung des Objektes, ab jetzt ist this gültig
+    this.initialize = function(){
+        // interne Struktur des gegebenen DIV anlegen mit 2 DIVs
+        plot_area
+            .css("background-color", "white")
+            .addClass('plot_diagram')               // Ermitteln aller aktiven Diagramme
+            .data('plot_diagram', this)             // Zugriff auf Objekt über DOM
+            .html('<div id="'+head_id+'" class="slick-shadow" style="float:left; width:100%; background-color: white; padding-bottom: 5px;"></div>'+
+            '<div id="'+canvas_id+'" class="slick-shadow" style="float:left; width:100%; height: '+canvas_height+'px; background-color: white; margin-bottom: 10px; "></div>'
+        )
+            .resize(function(){ resize_plot_diagrams();});     // Registrieren fuer Event
+        ;
+
+        // Header-Bereich belegen
+        jQuery('#'+head_id)
+            .html('<div style="float:left; padding:3px;">'+caption+'</div><div align = "right"><input class="close_diagram_'+unique_id+'" type="button" title="Diagramm Schliessen" value="X"></div>')
+            .css('margin-top', '5px')
+            .find(".close_diagram_"+unique_id).click(function(){remove_diagram()});
+
+        // Unterschiedliche IDs fuer Y-Achsen vergeben, wenn separat darzustellen
+        jQuery.each(data_array, function(i,val){
+            if (multiple_y_axes==true)
+                val.yaxis = data_array.length-i;
+            else
+                val.yaxis = 1;
+        });
+
+        plot_options = {
+            series:     {lines: { show: true }, points: { show: true }},
+            crosshair:  { mode: "x" },
+            grid:       { hoverable: true, autoHighlight: false },
+            yaxis:      { min: 0, show: show_y_axes==true },
+            legend:     { position: "ne"}
+        };
+        if (x_axis_time){
+            plot_options["xaxes"] = [{ mode: 'time' }]
+        }
+        plot = jQuery.plot(jQuery('#'+canvas_id), data_array, plot_options);     // Ausgabe des Diagrammes auf Canvas
+
+        if (data_array.length == 0){
+            return;                                   // Aufbereitung des Diagrammes verlassen wenn gar keine Daten zum Zeichnen
+        }
+
+
+        // ############ Context-Menü
+
+        var context_menu_id = "menu_"+canvas_id;
+        var menu = jQuery("<div class='contextMenu' id='"+context_menu_id+"' style='display:none;'>").insertAfter('#'+canvas_id);
+        var ul   = jQuery("<ul></ul>").appendTo(menu);
+        jQuery("<div id='header_"+context_menu_id+"' style='padding: 3px; background-color:lightgray;' align='center'>Diagram</div>").appendTo(ul);
+        var bindings = {};
+
+        function context_menu_entry(name, icon_class, label, hint, click_action ){
+            jQuery("<li id='"+context_menu_id+"_"+name+"' title='"+hint+"'><span class='"+icon_class+"' style='float:left'></span><span id='"+context_menu_id+"_"+name+"_label'>"+label+"</span></li>").appendTo(ul);
+            bindings[context_menu_id+"_"+name] = click_action;
+        }
+
+        context_menu_entry(
+            'y_axis',
+            "ui-icon ui-icon-comment",
+            show_y_axes==true ? locale_translate('diagram_y_axis_hide_name') : locale_translate('diagram_y_axis_show_name'),
+            show_y_axes==true ? locale_translate('diagram_y_axis_hide_hint') : locale_translate('diagram_y_axis_show_hint'),
+            function(t){
+                plot_area.html(""); // Altes Diagramm entfernen
+                plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_axes, (show_y_axes==true ? false : true), x_axis_time, locale);
+            }
+        );
+
+        context_menu_entry(
+            'all_in_one',
+            "ui-icon ui-icon-arrow-4-diag",
+            multiple_y_axes==true ? locale_translate('diagram_all_on_name') : locale_translate('diagram_all_off_name'),
+            multiple_y_axes==true ? locale_translate('diagram_all_on_hint') : locale_translate('diagram_all_off_hint'),
+            function(t){
+                plot_area.html(""); // Altes Diagramm entfernen
+                plot_diagram(unique_id, plot_area_id, caption, data_array, (multiple_y_axes==true ? false : true), show_y_axes, x_axis_time, locale);
+            }
+        );
+
+        jQuery('#'+canvas_id).contextMenu(context_menu_id, {
+            menuStyle: {  width: '330px' },
+            bindings:   bindings,
+            onContextMenu : function(event, menu)                                   // dynamisches Anpassen des Context-Menü
+            {
+                var cell = $(event.target);
+                return true;
+            }
+        });
+
+
+        this.registerLegend();  // erstmaliger Aufruf, des weiteren neuer Aufruf nach Resize
+    }   // end initialize
+
 
     function pad2(number){          // Vornullen auffuellen für Datum etc.
         var str=''+number;
@@ -25,122 +136,20 @@ function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_a
         plot_area.html("");                                  // Area putzen
     }
 
-    var canvas_id = "canvas_" + unique_id;
-    var head_id = "head_" + canvas_id;
-    var canvas_height = 450;
-
-    // interne Struktur des gegebenen DIV anlegen mit 2 DIVs
-    plot_area
-        .css("background-color", "white")
-        .html('<div id="'+head_id+'" class="slick-shadow" style="float:left; width:100%; background-color: white; padding-bottom: 5px;"></div>'+
-            '<div id="'+canvas_id+'" class="slick-shadow" style="float:left; width:100%; height: '+canvas_height+'px; background-color: white; margin-bottom: 10px; "></div>'
-        )
-    ;
-
-    // Header-Bereich belegen
-    jQuery('#'+head_id)
-        .html('<div style="float:left; padding:3px;">'+caption+'</div><div align = "right"><input class="close_diagram_'+unique_id+'" type="button" title="Diagramm Schliessen" value="X"></div>')
-        .css('margin-top', '5px')
-        .find(".close_diagram_"+unique_id).click(function(){remove_diagram()});
-
-    // Unterschiedliche IDs fuer Y-Achsen vergeben, wenn separat darzustellen
-    jQuery.each(data_array, function(i,val){
-        if (multiple_y_axes==true)
-            val.yaxis = data_array.length-i;
-        else
-            val.yaxis = 1;
-    });
-
-    plot_options = {
-        series:     {lines: { show: true }, points: { show: true }},
-        crosshair:  { mode: "x" },
-        grid:       { hoverable: true, autoHighlight: false },
-        yaxis:      { min: 0, show: show_y_axes==true },
-        legend:     { position: "ne"}
-    };
-    if (x_axis_time){
-        plot_options["xaxes"] = [{ mode: 'time' }]
-    }
-    var plot = jQuery.plot(jQuery('#'+canvas_id), data_array, plot_options);     // Ausgabe des Diagrammes auf Canvas
-
-    if (data_array.length == 0){
-        return;                                   // Aufbereitung des Diagrammes verlassen wenn gar keine Daten zum Zeichnen
+    // ############ MouseOver-Hint Anzeige aktualisieren
+    function showTooltip(x, y, contents) {
+        $('<div id="'+toolTipID+'">' + contents + '</div>').css( {
+            position: 'absolute',
+            display: 'none',
+            top: y + 5,
+            left: x + 5,
+            border: '1px solid #fdd',
+            padding: '2px',
+            'background-color': '#fee',
+            opacity: 0.80
+        }).appendTo("body").fadeIn(300);
     }
 
-
-    // ############ Context-Menü
-
-    var context_menu_id = "menu_"+canvas_id;
-    var menu = jQuery("<div class='contextMenu' id='"+context_menu_id+"' style='display:none;'>").insertAfter('#'+canvas_id);
-    var ul   = jQuery("<ul></ul>").appendTo(menu);
-    jQuery("<div id='header_"+context_menu_id+"' style='padding: 3px; background-color:lightgray;' align='center'>Diagram</div>").appendTo(ul);
-    var bindings = {};
-
-    function context_menu_entry(name, icon_class, label, hint, click_action ){
-        jQuery("<li id='"+context_menu_id+"_"+name+"' title='"+hint+"'><span class='"+icon_class+"' style='float:left'></span><span id='"+context_menu_id+"_"+name+"_label'>"+label+"</span></li>").appendTo(ul);
-        bindings[context_menu_id+"_"+name] = click_action;
-    }
-
-    context_menu_entry(
-        'y_axis',
-        "ui-icon ui-icon-comment",
-        show_y_axes==true ? locale_translate('diagram_y_axis_hide_name') : locale_translate('diagram_y_axis_show_name'),
-        show_y_axes==true ? locale_translate('diagram_y_axis_hide_hint') : locale_translate('diagram_y_axis_show_hint'),
-        function(t){
-            plot_area.html(""); // Altes Diagramm entfernen
-            plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_axes, (show_y_axes==true ? false : true), x_axis_time, locale);
-        }
-    );
-
-    context_menu_entry(
-        'all_in_one',
-        "ui-icon ui-icon-arrow-4-diag",
-        multiple_y_axes==true ? locale_translate('diagram_all_on_name') : locale_translate('diagram_all_off_name'),
-        multiple_y_axes==true ? locale_translate('diagram_all_on_hint') : locale_translate('diagram_all_off_hint'),
-        function(t){
-            plot_area.html(""); // Altes Diagramm entfernen
-            plot_diagram(unique_id, plot_area_id, caption, data_array, (multiple_y_axes==true ? false : true), show_y_axes, x_axis_time, locale);
-        }
-    );
-
-    jQuery('#'+canvas_id).contextMenu(context_menu_id, {
-        menuStyle: {  width: '330px' },
-        bindings:   bindings,
-        onContextMenu : function(event, menu)                                   // dynamisches Anpassen des Context-Menü
-        {
-            var cell = $(event.target);
-            return true;
-        }
-    });
-
-    // ############ crosshair.Anzeige aktualisieren
-    var updateLegendTimeout = null;
-    var latestPosition = null;
-
-    var legends = jQuery('#'+canvas_id+" .legendLabel");
-    legends.each(function () {
-        // fix the widths so they don't jump around
-        jQuery(this).css('width', jQuery(this).width()+10);
-    });
-
-    // Legendenzeile für X-Achse hinzufügen
-    var x_legend_title;
-    if (plot_options.xaxes[0].mode == "time"){
-        x_legend_title = "Time";
-    } else {
-        x_legend_title = 'X';
-    }
-    jQuery('#'+canvas_id+" .legend").find("table").addClass('legend_table');
-    jQuery('#'+canvas_id+" .legend").find("tbody").append("<tr><td align='center'>"+x_legend_title+"</td><td class='legendXAxis'></td></tr>");
-    var legendXAxis =jQuery('#'+canvas_id+" .legendXAxis");
-
-    // Titel zu Skalen der spalten hinzufuegen, wenn multiple y-Achsen angezeigt werden
-    if (multiple_y_axes==true){
-
-        jQuery.each(data_array, function(i,val){
-            jQuery('#'+canvas_id+" .y"+(data_array.length-i)+"Axis").attr("title", val.label);
-        });
-    }
 
     function updateLegend() {
         updateLegendTimeout = null;
@@ -181,45 +190,66 @@ function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_a
     }
 
 
-    // ############ MouseOver-Hint Anzeige aktualisieren
-    var previousToolTipPoint = null;
-    var toolTipID = canvas_id+"_ToolTip"
 
-    function showTooltip(x, y, contents) {
-        $('<div id="'+toolTipID+'">' + contents + '</div>').css( {
-            position: 'absolute',
-            display: 'none',
-            top: y + 5,
-            left: x + 5,
-            border: '1px solid #fdd',
-            padding: '2px',
-            'background-color': '#fee',
-            opacity: 0.80
-        }).appendTo("body").fadeIn(300);
+
+
+
+    this.registerLegend = function(){
+        updateLegendTimeout = null;                                             // Sicherstellen, dass Timeout neu aufgeseztz wird
+
+        // ############ Events binden
+        jQuery('#'+canvas_id).bind("plothover",  function (event, pos, item) {
+            latestPosition = pos;
+            if (!updateLegendTimeout)
+                updateLegendTimeout = setTimeout(updateLegend, 50);     // Zeitverzögertes Ausfrufen von updateLegend für crosshair und Aktualisierung Legende
+            if (item) {
+                if (previousToolTipPoint != item.dataIndex) {
+                    previousToolTipPoint = item.dataIndex;
+                    $("#"+toolTipID).remove();
+                    // Label ist schon durch Crosshair mit Anfangs-Wert belegt, diesen durch aktuellen ersetzen
+                    showTooltip(item.pageX, item.pageY, item.series.label + "= " + item.datapoint[1].toFixed(2)   );
+                }
+            }
+            else {
+                $("#"+toolTipID).remove();
+                previousToolTipPoint = null;
+            }
+
+        });
+
+
+        // ############ crosshair.Anzeige aktualisieren
+
+        legends             = jQuery('#'+canvas_id+" .legendLabel");
+
+        legends.each(function () {
+            // fix the widths so they don't jump around
+            jQuery(this).css('width', jQuery(this).width()+10);
+        });
+
+        // Legendenzeile für X-Achse hinzufügen
+        var x_legend_title;
+        if (plot_options.xaxes[0].mode == "time"){
+            x_legend_title = "Time";
+        } else {
+            x_legend_title = 'X';
+        }
+        jQuery('#'+canvas_id+" .legend").find("table").addClass('legend_table');
+        if (jQuery('#'+canvas_id+" .legendXAxis").length == 0)                  // bei erstmaligem aufruf Zeile hinzufügen
+            jQuery('#'+canvas_id+" .legend").find("tbody").append("<tr><td align='center'>"+x_legend_title+"</td><td class='legendXAxis'></td></tr>");
+        legendXAxis         =jQuery('#'+canvas_id+" .legendXAxis");             // merken für wiederholte Verwendung
+
+        // Titel zu Skalen der spalten hinzufuegen, wenn multiple y-Achsen angezeigt werden
+        if (multiple_y_axes==true){
+
+            jQuery.each(data_array, function(i,val){
+                jQuery('#'+canvas_id+" .y"+(data_array.length-i)+"Axis").attr("title", val.label);
+            });
+        }
+
+        jQuery('#'+canvas_id+" .legend").draggable().css("left", -9).css("top", canvas_height*-1+9); // Legende verschiebbar gestalten, da dann mit position:relative gearbeitet wird, muss neu positioniert werden
     }
 
-
-    // ############ Events binden
-    jQuery('#'+canvas_id).bind("plothover",  function (event, pos, item) {
-        latestPosition = pos;
-        if (!updateLegendTimeout)
-            updateLegendTimeout = setTimeout(updateLegend, 50);     // Zeitverzögertes Ausfrufen von updateLegend für crosshair und Aktualisierung Legende
-        if (item) {
-            if (previousToolTipPoint != item.dataIndex) {
-                previousToolTipPoint = item.dataIndex;
-                $("#"+toolTipID).remove();
-                // Label ist schon durch Crosshair mit Anfangs-Wert belegt, diesen durch aktuellen ersetzen
-                showTooltip(item.pageX, item.pageY, item.series.label + "= " + item.datapoint[1].toFixed(2)   );
-            }
-        }
-        else {
-            $("#"+toolTipID).remove();
-            previousToolTipPoint = null;
-        }
-
-    });
-
-    jQuery('#'+canvas_id+" .legend").draggable().css("left", -9).css("top", canvas_height*-1+9); // Legende verschiebbar gestalten, da dann mit position:relative gearbeitet wird, muss neu positioniert werden
 
 
     /**
@@ -278,5 +308,16 @@ function plot_diagram(unique_id, plot_area_id, caption, data_array, multiple_y_a
         }
     }
 
-
 } // plot_diagram
+
+
+function resize_plot_diagrams(){
+    jQuery('.plot_diagram').each(function(index, element) {
+            console.log('resizeDiagram');
+            var Container = jQuery(element);
+            Container.data('plot_diagram').registerLegend();
+        }
+    );
+}
+
+
