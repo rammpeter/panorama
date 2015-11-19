9ca06ec83f2e4b8052ef4ef9f762463148544872
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-11-19 14:55:10 +0100

    Switch from cookie to server-storage for client-info

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.1.8'
-  RELEASE_DATE = Date.parse('2015-11-16')
+  VERSION = '2.1.9'
+  RELEASE_DATE = Date.parse('2015-11-19')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
