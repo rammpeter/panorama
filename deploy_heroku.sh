43ce3150617f391502a3aebee07f9135deb174fa
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2015-12-02 17:26:58 +0100

    Test instance at heroku

---
+++ deploy_heroku.sh
@@ -0,0 +1,10 @@
+# Deployment nach Heroku
+
+# Schritte zur Einrichtung, Beschreibung unter https://github.com/heroku/heroku-deploy
+# > Installation ToolBelt
+# > heroku plugins:install https://github.com/heroku/heroku-deploy
+# > heroku create
+# Letzte URL war: https://boiling-plains-4451.herokuapp.com/
+
+heroku deploy:war --war Panorama.war 
+
