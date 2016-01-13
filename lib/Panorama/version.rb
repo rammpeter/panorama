7646fe9ce54737357e4124a1370a2eef721d10c1
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-01-13 09:16:15 +0100

    Remove useless div before legend_table

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.1.26'
-  RELEASE_DATE = Date.parse('2016-01-09')
+  VERSION = '2.1.27'
+  RELEASE_DATE = Date.parse('2016-01-13')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
