73e207b39f570fc8a28202b1b65a25db003b2e1e
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-01-29 11:21:33 +0100

    Tablespace-usage

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.1.36'
-  RELEASE_DATE = Date.parse('2016-01-27')
+  VERSION = '2.1.37'
+  RELEASE_DATE = Date.parse('2016-01-29')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
