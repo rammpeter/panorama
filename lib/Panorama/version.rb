a27daac15ef147900ac6beec8531db8de9910fa3
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-03-06 16:25:05 +0100

    Check with has_key for existence in hash

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,6 +1,6 @@
 module Panorama
-  VERSION = '2.1.53'
-  RELEASE_DATE = Date.parse('2016-03-05')
+  VERSION = '2.1.54'
+  RELEASE_DATE = Date.parse('2016-03-06')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
