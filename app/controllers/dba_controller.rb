d876f6d1ea726d0e20792e743daf545936dec8ae
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-03-13 17:54:31 +0100

    Translation

--- app/controllers/dba_controller.rb
+++ app/controllers/dba_controller.rb
@@ -1050,8 +1050,8 @@ Möglicherweise fehlende Zugriffsrechte auf Table X$BH! Lösung: Exec als User '
     @column_options << {:caption=>"Owner",       :data=>"rec.owner",               :title=>"Object-Owner"}
     @column_options << {:caption=>"Name",        :data=>"rec.object_name",         :title=>"Object-Name"}
     @column_options << {:caption=>"Sub-Name",    :data=>"rec.subobject_name",      :title=>"Sub-Object-Name"} if @show_partitions
-    @column_options << {:caption=>"Sample",      :data=>proc{|rec| formattedNumber(rec.sample)}, :title=>"Statistik-Wert innerhalb der Sample-Dauer",    :align=>"right"}
-    @column_options << {:caption=>"Total",       :data=>proc{|rec| formattedNumber(rec.total)},  :title=>"Statistik-Wert global sei Instance-Start",     :align=>"right"}
+    @column_options << {:caption=>"Sample",      :data=>proc{|rec| formattedNumber(rec.sample)}, :title=>t(:dba_show_segment_statistics_sample_hint, :default=>'Statistics-value within the sample time'),    :align=>"right"}
+    @column_options << {:caption=>"Total",       :data=>proc{|rec| formattedNumber(rec.total)},  :title=>t(:dba_show_segment_statistics_total_hint, :default=>'Statistics-value cumulated since instance startup'),     :align=>"right"}
 
     data1 = get_values    # Snapshot vor SampleTime
     sampletime = params[:sample_length].to_i
