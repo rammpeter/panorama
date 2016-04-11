5c4b7a0e383e15e5bbf7615fd01694799d736f03
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-04-11 16:07:29 +0200

    Version

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,7 +1,7 @@
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.1.69'
-  RELEASE_DATE = Date.parse('2016-04-06')
+  VERSION = '2.1.70'
+  RELEASE_DATE = Date.parse('2016-04-11')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
