a5622c6d08f05a06b72d0a164df1304838d31044
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-04-26 14:20:57 +0200

    Bugfix historic SQLs using object

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,7 +1,7 @@
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.1.77'
-  RELEASE_DATE = Date.parse('2016-04-25')
+  VERSION = '2.1.78'
+  RELEASE_DATE = Date.parse('2016-04-26')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
