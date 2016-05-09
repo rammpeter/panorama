ad72ed6aab4d651387990f76638991d671f99be9
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-05-09 15:37:01 +0200

    Show max. amount of TEMP and PGA in execution plan of SGA

--- lib/Panorama/version.rb
+++ lib/Panorama/version.rb
@@ -1,7 +1,7 @@
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.1.79'
-  RELEASE_DATE = Date.parse('2016-04-28')
+  VERSION = '2.1.80'
+  RELEASE_DATE = Date.parse('2016-05-09')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
