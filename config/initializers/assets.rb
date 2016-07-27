af942593c84a3b0d1f6fb38bb88b863a81fb8658
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-07-27 16:26:26 +0200

    Dependency mgmt. in war-file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 
-Rails.application.config.assets.precompile += %w( favicon.ico )
+Rails.application.config.assets.precompile += %w( *.ico *.gif *.png *.jpg )
+
+
+
+
