b176e648bf72149779f531a67657e2b9ab7565b6
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2016-10-25 17:28:52 +0200

    Complete switch to rails 5

--- lib/Panorama/engine.rb
+++
@@ -1,7 +0,0 @@
-module Panorama
-  class Engine < ::Rails::Engine
-    # Keine Namespace-Isolation für Engine verwenden
-    # isolate_namespace Panorama
-  end
-end
-
--- lib/panorama/version.rb
+++ lib/panorama/version.rb
@@ -1,7 +1,7 @@
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.2.28'
-  RELEASE_DATE = Date.parse('2016-10-19')
+  VERSION = '2.3.1'
+  RELEASE_DATE = Date.parse('2016-10-20')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
