a521572d5cc90e3b5adab01cd7794f224934c029
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2021-03-08 13:49:07 +0100

    cookies_same_site_protection = :lax

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -3,3 +3,9 @@
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
+
+# Specify cookies SameSite protection level: either :none, :lax, or :strict.
+#
+# This change is not backwards compatible with earlier Rails versions.
+# It's best enabled when your entire app is migrated and stable on 6.1.
+Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
