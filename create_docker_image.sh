064a1e992cf13ed9fda44ade88722c2052d1c786
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2022-07-13 11:13:41 +0200

    pull base images before build

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -1,7 +1,7 @@
 # Deploy Panorama to Dockerhub.com and Dockerhub.osp-dd.de
 # Peter Ramm, 21.09.2019
 
-for FROM in `grep "^FROM" Dockerfile | awk '{print $2}' | tail -n1`; do
+for FROM in `grep "^FROM" Dockerfile | grep -v scratch |  awk '{print $2}' | tail -n1`; do
   echo "Ensure using latest version of $FROM"
   docker pull $FROM
 done
@@ -9,7 +9,8 @@ done
 JRUBY_VERSION=`cat .ruby-version | cut -c 7-13`
 echo JRUBY_VERSION=$JRUBY_VERSION
 
-docker build --build-arg JRUBY_VERSION=$JRUBY_VERSION --progress=plain -t rammpeter/panorama .
+# Use "--progress=plain" to see the full console output
+docker build --build-arg JRUBY_VERSION=$JRUBY_VERSION -t rammpeter/panorama .
 RC=$?
 if [ $RC -ne 0 ]
 then
