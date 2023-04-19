6aa14c22027499a1bf92ef584c599f4105c12c87
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2023-04-19 15:03:15 +0200

    Exclude /vendor from production

--- config/jarble.rb
+++ config/jarble.rb
@@ -5,14 +5,21 @@
 Jarbler::Config.new do |config|
   # Name of the generated jar file 
   # config.jar_name = 'Panorama.jar'
- 
+
   # Application directories or files to include in the jar file
-  # config.includes = ["app", "bin", "config", "config.ru", "db", "Gemfile", "Gemfile.lock", "lib", "log", "script", "vendor", "tmp"]
- 
+  # config.includes = ["app", "bin", "config", "config.ru", "db", "Gemfile", "Gemfile.lock", "lib", "log", "public", "script", "vendor", "tmp"]
+  # config.includes << 'additional'
+
   # Application directories or files to exclude from the jar file
-  # config.excludes = []
+  # config.excludes = ["tmp/cache", "tmp/pids", "tmp/sockets", "vendor/bundle", "vendor/cache", "vendor/ruby"]
+  # vendor/assets is not needed because duplicate in public
+  config.excludes << 'vendor'
 
   # The network port used by the application
   config.port = 8080
- 
+
+  # jRuby version to use if not the latest or the version from .ruby-version is used
+  # config.jruby_version = '9.2.3.0'
+  # config.jruby_version = ''
+
 end
