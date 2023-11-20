7284c3f163913b130e473b1a4ed677ae6cc36a26
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2023-11-20 11:39:38 +0100

    Limit lifetime of session cookie to 8 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_panorama_session'
+Rails.application.config.session_store :cookie_store, key: '_panorama_session', expire_after: 8.hours
