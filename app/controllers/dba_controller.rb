2b89a0121d948682b1a3dc9440e1f5297eaf13e0
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2023-11-23 15:45:03 +0100

    Show DB's SYSDATE instead of Panorama's server time

--- app/controllers/dba_controller.rb
+++ app/controllers/dba_controller.rb
@@ -947,6 +947,7 @@ oradebug setorapname diag
                   s.UserName, s.Machine, s.OSUser, s.Process, s.Program,
                   SYSDATE - (s.Last_Call_Et/86400) Last_Call,
                   s.Logon_Time,
+                  SYSDATE,
                   sci.Network_Encryption, sci.Network_Checksumming,
                   p.spID, p.PID,
                   RawToHex(tx.XID) Tx_ID,
