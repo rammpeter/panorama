bba6b61466d7c29a840b721747037c89ca7d78f7
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2024-01-31 10:11:24 +0100

    Fix tests usage info housekeeping

--- test/models/usage_info_test.rb
+++ test/models/usage_info_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class UsageInfoTest < ActiveSupport::TestCase
   test "housekeeping_usage_info" do
+    ClientInfoStore.write(:dummy, :value)                                       # Ensure existence of client_info_store file before test
     UsageInfo.housekeeping
   end
 end
