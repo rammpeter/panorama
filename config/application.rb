41806bcfdd658d3719d0fa76485a3df911e9abd3
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2024-05-23 10:54:17 +0200

    Bugfix list gradual password rollover

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.17.42'
-  RELEASE_DATE = Date.parse('2024-04-24')
+  VERSION = '2.17.43'
+  RELEASE_DATE = Date.parse('2024-05-23')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
