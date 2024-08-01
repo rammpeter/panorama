a1799c2f003a6454ef1c981211ed109606f7ad9b
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2024-08-01 11:29:23 +0200

    Session wait info extended PL/SQL object also shown for wrapped SYS packages

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.17.48'
-  RELEASE_DATE = Date.parse('2024-07-02')
+  VERSION = '2.17.49'
+  RELEASE_DATE = Date.parse('2024-08-01')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
