91aa2baa398b3c8b8d110efa46851c56df52ae4d
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2024-11-23 10:35:26 +0100

    Fix problem with Panorama-Sampler object creation.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.17.59'
+  VERSION = '2.17.60'
   RELEASE_DATE = Date.parse('2024-11-21')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
