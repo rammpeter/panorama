707afb0a677a7dab34eb20373bc45b82b9a43d14
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2025-04-27 06:05:28 +0200

    Compile Java with Java 8 compatibility

--- config/jarble.rb
+++ config/jarble.rb
@@ -8,10 +8,13 @@ Jarbler::Config.new do |config|
   # Name of the generated jar file 
   config.jar_name = 'Panorama.jar'
 
+  # Compile JarMain class so that the resulting Panorama.jar is compatible with Java 8 and above
+  # Works only up to JRuby < 10.0.0.0 which requires Java 21 or above
+  config.java_opts = '-source 1.8 -target 1.8'
 
   # Application directories or files to include in the jar file
   # config.includes = ["app", "bin", "config", "config.ru", "db", "Gemfile", "Gemfile.lock", "lib", "log", "public", "script", "vendor", "tmp"]
-  # config.includes << 'additional'
+  # config.includes << "additional"
 
   # Application directories or files to exclude from the jar file
   # config.excludes = ["tmp/cache", "tmp/pids", "tmp/sockets", "vendor/bundle", "vendor/cache", "vendor/ruby"]
@@ -19,7 +22,7 @@ Jarbler::Config.new do |config|
   config.excludes << 'vendor'
 
   # jRuby version to use if not the latest or the version from .ruby-version is used
-  # config.jruby_version = '9.2.3.0'
+  # config.jruby_version = "9.2.3.0"
   # config.jruby_version = ''
 
 end
