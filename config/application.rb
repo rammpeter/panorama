919698e3a9391342479f368e3adb5706bfc04520
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2025-06-19 15:40:45 +0200

    Release++

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Panorama
   # VERSION and RELEASE_DATE should have fix syntax and positions because they are parsed from other sites
-  VERSION = '2.17.74'
-  RELEASE_DATE = Date.parse('2025-05-12')
+  VERSION = '2.17.75'
+  RELEASE_DATE = Date.parse('2025-06-19')
 
   RELEASE_DAY   = "%02d" % RELEASE_DATE.day
   RELEASE_MONTH = "%02d" % RELEASE_DATE.month
