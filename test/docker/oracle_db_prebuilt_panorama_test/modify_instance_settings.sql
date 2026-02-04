78902fc2af7d5f1860f6d070a12e8c999266daa4
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2026-02-04 14:59:20 +0100

    Test: disable recycle bin for test DBs

--- test/docker/oracle_db_prebuilt_panorama_test/modify_instance_settings.sql
+++ test/docker/oracle_db_prebuilt_panorama_test/modify_instance_settings.sql
@@ -2,5 +2,6 @@
 -- CAUTION: for XE Memory settings MUST be < 2 GB, otherwise no startup nomount is possible
 ALTER SYSTEM SET SGA_MAX_SIZE=1500M scope=spfile;
 ALTER SYSTEM SET SGA_TARGET=1500M scope=spfile;
+ALTER SYSTEM SET recyclebin = OFF SCOPE = SPFILE;
 SHUTDOWN IMMEDIATE;
 STARTUP;
