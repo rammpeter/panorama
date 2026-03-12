943bf5a902e70f2e044b89e449837e6d55db40ce
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2026-03-12 09:22:28 +0100

    Test: don't catch specific exception class because Java::OracleJdbc::OracleDatabaseException was not catched by class

--- lib/test_helpers/oracle_connection_test_helper.rb
+++ lib/test_helpers/oracle_connection_test_helper.rb
@@ -87,7 +87,9 @@ class ActiveSupport::TestCase
 
     begin
       snapshots = sql_select_one "SELECT COUNT(*) FROM Panorama_Snapshot"
-    rescue ActiveRecord::StatementInvalid, Java::OracleJdbc::OracleDatabaseException   # Table does not yet exist
+    rescue                                                                      # Table does not yet exist
+      # Don't catch specific class because Java::OracleJdbc::OracleDatabaseException seems not to be catchable by rescue
+      Rails.logger.warn('ensure_panorama_sampler_tables_exist_with_content') { ""}
       PanoramaSamplerStructureCheck.do_check(sampler_config, :ASH)              # Must be first if tables do not exist, but packages
       PanoramaSamplerStructureCheck.do_check(sampler_config, :AWR)              # AWR package depends on ASH tables, so check ASH first
       snapshots = sql_select_one "SELECT COUNT(*) FROM Panorama_Snapshot"
