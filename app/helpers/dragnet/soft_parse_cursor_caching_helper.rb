c44fb70e8fb12e084424f8b38015fcd6d26f8a5e
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2026-05-12 16:42:34 +0200

    Fix dragnet sql

--- app/helpers/dragnet/soft_parse_cursor_caching_helper.rb
+++ app/helpers/dragnet/soft_parse_cursor_caching_helper.rb
@@ -31,25 +31,24 @@ module Dragnet::SoftParseCursorCachingHelper
         sql:  "\
 SELECT SUM(x.Parse_Calls)       Parse_Calls_Total,
        SUM(x.Executions)        Executions_Total,
-       u.UserName, x.Max_ASH_Module, x.Max_ASH_Action, x.Max_ASH_Machine, sv.Name Max_ASH_Service_Name,
+       ROUND(SUM(x.Parse_Calls) * 100 / SUM(x.Executions), 1) \"Parse pct. of execs\",
+       u.UserName, x.Max_ASH_Module, x.Max_ASH_Machine, sv.Name Max_ASH_Service_Name,
        SUM(x.Samples)           ASH_Samples,
        COUNT(DISTINCT SQL_ID)   Distinct_SQL_Statements,
        MAX(Max_Parses_SQL_ID)   Max_Parses_SQL_ID
-FROM   (SELECT s.Inst_ID, s.SQL_ID, h.Samples, h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Action, h.Max_ASH_Machine, h.Max_ASH_Service_Hash,
+FROM   (SELECT s.Inst_ID, s.SQL_ID, h.Samples, h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Machine, h.Max_ASH_Service_Hash,
                s.Executions, s.Parse_Calls,
-               MAX(s.SQL_ID)  OVER (PARTITION BY h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Action, h.Max_ASH_Machine, h.Max_Ash_Service_Hash ORDER BY s.Parse_Calls DESC) Max_Parses_SQL_ID
+               MAX(s.SQL_ID)  OVER (PARTITION BY h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Machine, h.Max_Ash_Service_Hash ORDER BY s.Parse_Calls DESC) Max_Parses_SQL_ID
         FROM   gv$SQLArea s
         JOIN   (SELECT Inst_ID, SQL_ID, SUM(Samples) Samples,
                         MAX(Max_ASH_User_ID)      Max_ASH_User_ID,
                         MAX(Max_ASH_Module)       Max_ASH_Module,
-                        MAX(Max_ASH_Action)       Max_ASH_Action,
                         MAX(Max_ASH_Machine)      Max_ASH_Machine,
                         MAX(Max_ASH_Service_Hash) Max_ASH_Service_Hash
                 FROM   (
                         SELECT /*+ NO_MERGE */ Inst_ID, SQL_ID, Module, Action, Machine, Service_Hash, COUNT(*) Samples,
                                MAX(User_ID)      OVER (PARTITION BY Inst_ID, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_User_ID,
                                MAX(Module)       OVER (PARTITION BY Inst_ID, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Module,
-                               MAX(Action)       OVER (PARTITION BY Inst_ID, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Action,
                                MAX(Machine)      OVER (PARTITION BY Inst_ID, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Machine,
                                MAX(Service_Hash) OVER (PARTITION BY Inst_ID, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Service_Hash
                         FROM   gv$Active_Session_History h
@@ -62,12 +61,12 @@ FROM   (SELECT s.Inst_ID, s.SQL_ID, h.Samples, h.Max_ASH_User_ID, h.Max_ASH_Modu
        ) x
 JOIN   gv$Services sv ON sv.Inst_ID = x.Inst_ID AND sv.Name_Hash = x.Max_ASH_Service_Hash
 JOIN   All_Users u    ON u.User_ID = x.Max_ASH_User_ID
-GROUP BY u.UserName, x.Max_ASH_Module, x.Max_ASH_Action, x.Max_ASH_Machine, sv.Name
-HAVING SUM(Parse_Calls) * 100 / SUM(Executions) > ?
+GROUP BY u.UserName, x.Max_ASH_Module, x.Max_ASH_Machine, sv.Name
+HAVING SUM(x.Parse_Calls) * 100 / SUM(x.Executions) > ?
 ORDER BY Parse_Calls_Total DESC
         ",
         parameter: [
-          {name: t(:dragnet_helper_156_param2_name, default: 'Program name in ASH'), size: 30, default: 'JDBC Thin Client', title: t(:dragnet_helper_156_param2_hint, default: 'The used program name in Active Session History. If not explicitely set the default is "JDBC Thin Client"') },
+          {name: t(:dragnet_helper_156_param2_name, default: 'Program name in ASH'), size: 30, default: 'JDBC Thin Client', title: t(:dragnet_helper_156_param2_hint, default: 'The recorded program name in Active Session History. If not explicitely set the default is "JDBC Thin Client"') },
           {name: t(:dragnet_helper_156_param1_name, default: 'Min. parse count % of execution count'), size: 8, default: 10, title: t(:dragnet_helper_156_param1_hint, default: 'Minimum number of parse counts in % compared to the execution count') },
         ]
       },
@@ -77,25 +76,24 @@ ORDER BY Parse_Calls_Total DESC
         sql:  "\
 SELECT SUM(x.Parse_Calls_Delta)       Parse_Calls_Total,
        SUM(x.Executions_Delta)        Executions_Total,
-       x.DBID, u.UserName, x.Max_ASH_Module, x.Max_ASH_Action, x.Max_ASH_Machine, sv.Name Max_ASH_Service_Name,
+       ROUND(SUM(x.Parse_Calls_Delta) * 100 / SUM(x.Executions_Delta), 1) \"Parse pct. of execs\",
+       x.DBID, u.UserName, x.Max_ASH_Module, x.Max_ASH_Machine, sv.Name Max_ASH_Service_Name,
        SUM(x.Samples)           ASH_Samples,
        COUNT(DISTINCT SQL_ID)   Distinct_SQL_IDs,
        MAX(Max_Parses_SQL_ID)   Max_Parses_SQL_ID
-FROM   (SELECT s.DBID, s.Instance_Number, s.SQL_ID, h.Samples, h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Action, h.Max_ASH_Machine, h.Max_ASH_Service_Hash,
+FROM   (SELECT s.DBID, s.Instance_Number, s.SQL_ID, h.Samples, h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Machine, h.Max_ASH_Service_Hash,
                s.Executions_Delta, s.Parse_Calls_Delta,
-               MAX(s.SQL_ID)  OVER (PARTITION BY h.DBID, h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Action, h.Max_ASH_Machine, h.Max_Ash_Service_Hash ORDER BY s.Parse_Calls_Delta DESC) Max_Parses_SQL_ID
+               MAX(s.SQL_ID)  OVER (PARTITION BY h.DBID, h.Max_ASH_User_ID, h.Max_ASH_Module, h.Max_ASH_Machine, h.Max_Ash_Service_Hash ORDER BY s.Parse_Calls_Delta DESC) Max_Parses_SQL_ID
         FROM   DBA_Hist_SQLStat s
         JOIN   (SELECT DBID, Instance_Number, SQL_ID, SUM(Samples) Samples,
                         MAX(Max_ASH_User_ID)      Max_ASH_User_ID,
                         MAX(Max_ASH_Module)       Max_ASH_Module,
-                        MAX(Max_ASH_Action)       Max_ASH_Action,
                         MAX(Max_ASH_Machine)      Max_ASH_Machine,
                         MAX(Max_ASH_Service_Hash) Max_ASH_Service_Hash
                 FROM   (
                         SELECT /*+ NO_MERGE */ h.DBID, h.Instance_Number, SQL_ID, Module, Action, Machine, Service_Hash, COUNT(*) Samples,
                                MAX(User_ID)      OVER (PARTITION BY h.DBID, h.Instance_Number, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_User_ID,
                                MAX(Module)       OVER (PARTITION BY h.DBID, h.Instance_Number, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Module,
-                               MAX(Action)       OVER (PARTITION BY h.DBID, h.Instance_Number, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Action,
                                MAX(Machine)      OVER (PARTITION BY h.DBID, h.Instance_Number, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Machine,
                                MAX(Service_Hash) OVER (PARTITION BY h.DBID, h.Instance_Number, SQL_ID ORDER BY COUNT(*) DESC) Max_ASH_Service_Hash
                         FROM   DBA_Hist_Active_Sess_History h
@@ -111,13 +109,13 @@ FROM   (SELECT s.DBID, s.Instance_Number, s.SQL_ID, h.Samples, h.Max_ASH_User_ID
        ) x
 LEFT OUTER JOIN   gv$Services sv ON sv.Inst_ID = x.Instance_Number AND sv.Name_Hash = x.Max_ASH_Service_Hash
 LEFT OUTER JOIN   All_Users u    ON u.User_ID = x.Max_ASH_User_ID
-GROUP BY x.DBID, u.UserName, x.Max_ASH_Module, x.Max_ASH_Action, x.Max_ASH_Machine, sv.Name
+GROUP BY x.DBID, u.UserName, x.Max_ASH_Module, x.Max_ASH_Machine, sv.Name
 HAVING SUM(Parse_Calls_Delta)*100 / SUM(Executions_Delta) > ?
 ORDER BY Parse_Calls_Total DESC
         ",
         parameter: [
           {name: t(:dragnet_helper_param_history_backward_name, default: 'Consideration of history backward in days'), size: 8, default: 8, title: t(:dragnet_helper_param_history_backward_hint, default: 'Number of days in history backward from now for consideration') },
-          {name: t(:dragnet_helper_156_param2_name, default: 'Program name in ASH'), size: 30, default: 'JDBC Thin Client', title: t(:dragnet_helper_156_param2_hint, default: 'The used program name in Active Session History. If not explicitely set the default is "JDBC Thin Client"') },
+          {name: t(:dragnet_helper_156_param2_name, default: 'Program name in ASH'), size: 30, default: 'JDBC Thin Client', title: t(:dragnet_helper_156_param2_hint, default: 'The recorded program name in Active Session History. If not explicitely set the default is "JDBC Thin Client"') },
           {name: t(:dragnet_helper_156_param1_name, default: 'Min. parse count % of execution count'), size: 8, default: 10, title: t(:dragnet_helper_156_param1_hint, default: 'Minumum number of parse counts in % compared to the execution count') }
         ]
       },
@@ -125,10 +123,9 @@ ORDER BY Parse_Calls_Total DESC
   end
 
   def jdbc_statement_cache_desc_default
-    'The number of soft parses on JDBC thin connections suggests that client-side JDBC statement caching is not being used or the statement cache setting may be too low.
-     JDBC client-side statement caching allows to keep a number of frequently used SQL cursors open at the DB connection even if the JDBC statement handle in the application is closed.
-     This allows to improve application performance and reduce soft parse activity even if application itself is unable to keep frequently used cursors open.
-     To be unable to keep frequently used cursors open is often the case if the application uses OR-mapper frameworks.
+    'High soft parse rate on JDBC Thin connections suggests JDBC client-side statement caching is disabled or undersized.
+     Enabling it keeps frequently used cursors open on the connection even after the application closes its statement handles.
+     Especially relevant for applications using OR-mapper frameworks, which typically cannot keep cursors open themselves.
 
      The JDBC statement cache is disabled per default in Oracle\'s JDBC driver.
      There are two ways to activate JDBC client-side statement caching and set cache size (to 100 for example):
