4d47eee97d25ce0d26b804b7d46bd6cbd1eddaa3
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2026-08-31 17:58:06 +0200

    Fix: Use CDB_Unified_Audit_Trail for >12.2 only, access fails on 12.2

--- app/helpers/dba_schema_helper.rb
+++ app/helpers/dba_schema_helper.rb
@@ -141,7 +141,7 @@ For exact values click for calculation with DBMS_SPACE.SPACE_USAGE."
   # Decide which view name to use for unified audit trail depending on the database version and mode (CDB or non-CDB).
   # @return [String] The view name for unified audit trail to use
   def unified_audit_trail_view_name
-    if get_db_version > '12' && PanoramaConnection.is_cdb? && PanoramaConnection.con_id == 1
+    if get_db_version >= '18' && PanoramaConnection.is_cdb? && PanoramaConnection.con_id == 1
       'CDB_Unified_Audit_Trail'
     else
       'Unified_Audit_Trail'
